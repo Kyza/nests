a62109645473ac94932809994ce6ee2d194455e7
Kyza <kyza@kyza.net>
2021-09-23 18:20:52 -0400

    Remove extra files

--- scripts/build.mjs
+++ scripts/build.mjs
@@ -8,7 +8,7 @@ await fs.emptyDir("lib");
 // Build all.
 await Promise.all([
 	$`npx tsc --project tsconfig.cjs.json`,
-	$`npx tsc --project tsconfig.esm.json`,
+	$`npx tsc --project tsconfig.json`,
 	$`npx tsc --project tsconfig.mjs.json`,
 	$`npx tsc --project tsconfig.types.json`,
 ]);
@@ -19,12 +19,8 @@ await $`npx renamer --find ".js" --replace ".mjs" "**"`;
 cd("../..");
 
 // Copy over all the types.
-// Copy over the README.md, LICENSE, and package.json.
 await Promise.all([
 	fs.copySync("lib/types", "lib/cjs"),
 	fs.copySync("lib/types", "lib/esm"),
 	fs.copySync("lib/types", "lib/mjs"),
-	fs.copyFile("package.json", "lib/package.json"),
-	fs.copyFile("LICENSE", "lib/LICENSE"),
-	fs.copyFile("README.md", "lib/README.md"),
 ]);
